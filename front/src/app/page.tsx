6be8a8a8ca6b43ed3aa42fd65b1f6c683cd87c75
GaspardPcht <gaspardpauchet@gmail.com>
2024-12-12 20:21:22 +0100

    Ajout des cartes avec effet de rotation 3D et intégration des icônes React

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,27 +1,23 @@
-'use client'
-import React from "react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import TiltCard from '../../components/TiltCards';
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
   const handleConnection = () => {
-    router.push('/auth/signin') 
-  }
+    router.push('/auth/signin');
+  };
   const handleInscription = () => {
-    router.push('/auth/signup') 
-  }
-  
+    router.push('/auth/signup');
+  };
+
   return (
     <div className="bg-white min-h-screen">
       <header className="flex items-center justify-between px-8 py-4 border-b">
         {/* Logo */}
         <div className="text-2xl font-bold">
-          <img
-            src="/Logo/logo.png"
-            alt="Rendezio Logo"
-            className="h-16 w-16"
-          />
+          <img src="/Logo/logo.png" alt="Rendezio Logo" className="h-16 w-16" />
         </div>
         {/* Navigation */}
         <nav className="flex items-center space-x-6">
@@ -29,14 +25,28 @@ export default function Home() {
             <a className="text-gray-700 hover:text-blue-500">Entreprise</a>
           </Link>
           {/* Connexion */}
-          <button onClick={handleConnection} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          <button
+            onClick={handleConnection}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
             Connexion
           </button>
-          <button onClick={handleInscription} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          <button
+            onClick={handleInscription}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
             Inscription
           </button>
         </nav>
       </header>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex items-center justify-center space-x-8 w-full">
+          <h1 className="text-6xl text-[#00263B] font-bold">BookSafe BookNow</h1>
+          <div className="flex text-black">
+            <TiltCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
