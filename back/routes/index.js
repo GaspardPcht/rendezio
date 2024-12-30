99f8bd999277e5663109fc71e0014c23c1506df1
GaspardPcht <gaspardpauchet@gmail.com>
2024-12-30 11:59:50 +0100

    add router index

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.get('/', (req, res) => {
+  res.json({ message: 'Bienvenue sur l\'API !' });
+});
 
 module.exports = router;
