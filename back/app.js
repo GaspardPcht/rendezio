86004ac4b7a4824629f57a4d168412cf102ddd0a
GaspardPcht <gaspardpauchet@gmail.com>
2024-12-30 11:52:25 +0100

    change dotenv on app.js

--- back/app.js
+++ back/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: './env/.env' });
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
