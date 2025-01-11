2eb50807202a9d506fa19a3a82a5777572a6e13c
GaspardPauchet <gaspardpauchet@gmail.com>
2025-01-11 16:08:24 +0100

    change

--- back/routes/index.js
+++ back/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Bienvenue sur l\'API !' });
+  res.json({ message: 'Bienvenue sur l\'API bg !' });
 });
 
 module.exports = router;
